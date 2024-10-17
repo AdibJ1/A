56191a1d172f2ac5435b59fc287c100ed53fbff1
AdibJ1 <adib.junaid@mail.bcu.ac.uk>
2024-10-17 22:45:36 +0100

    Add files via upload

---
+++ new_ssh.py
@@ -0,0 +1,51 @@
+import pexpect
+
+ip_address = '192.168.56.101'
+username = 'prne'
+password = 'cisco123!'
+password_enable = 'class123!'
+
+session = pexpect.spawn('ssh ' + username + '@' + ip_address, encoding='utf-8', timeout=20)
+result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
+
+if result != 0:
+    print('--- FAILURE! creating session for: ', ip_address)
+    exit()
+
+session.sendline(password)
+result = session.expect(['>', pexpect.TIMEOUT, pexpect.EOF])
+
+if result != 0:
+    print('--- FAILURE! entering password: ', password)
+    exit()
+
+session.sendline('enable')
+result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
+
+if result != 0:
+    print('--- FAILURE! entering enable mode')
+    exit()
+
+session.sendline(password_enable),
+result = session.expect(['#', pexpect.TIMEOUT, pexpect.EOF])
+
+if result != 0:
+    print('--- FAILURE! entering enable mode after sending password')
+    exit()
+
+session.sendline('configure terminal')
+result = session.expect([r'.\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
+
+if result != 0:
+    print('--- FAILURE! em=ntering config mode')
+    exit()
+
+session.sendline('hostname R1')
+result = session.expect([r'R1\(config\)#', pexpect.TIMEOUT, peXPECt.EOF])
+
+if result != 0:
+    print('--- FAILURE! setting hostname')
+
+session.sendline('exit')
+
+session.sendline('exit')
