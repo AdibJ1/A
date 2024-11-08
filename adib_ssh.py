1b1dfb6d8d0c0c83f6814b22e091b2aaf96e204f
AdibJ1 <adib.junaid@mail.bcu.ac.uk>
2024-11-08 12:47:02 +0000

    Update adib_ssh.py

--- adib_ssh.py
+++ adib_ssh.py
@@ -1,9 +1,25 @@
 import pexpect
+import re
 
 ip_address = '192.168.56.101'
 username = 'prne'
 password = 'cisco123!'
 password_enable = 'class123!'
+
+hardening_checklist = {
+    'SSH enabled': 'ip ssh version 2',
+    'Telnet disabled': 'no service telnet',
+    'Password encryption': 'service password-encryption',
+    'Logging enabled': 'logging buffered',
+    'NTP configured': 'ntp server',
+}
+
+def check_hardening(running_config):
+    for check, rule in hardening_checklist.items():
+    if rule in running_config:
+        print(f'[SUCCESSFUL] {check}')
+    else:
+        print(f'[FAILURE] {check}')
 
 session = pexpect.spawn('ssh ' + username + '@' + ip_address, encoding='utf-8', timeout=20)
 result = session.expect(['Password:', pexpect.TIMEOUT, pexpect.EOF])
@@ -42,6 +58,13 @@ if result != 0:
     print('--- FAILURE! setting hostname')
     
 session.sendline('exit')
+
+session.sendline('show running-config')
+result = session.expect([r'#', pexpect.TIMEOUT,pexpect.EOF])
+if result != 0:
+    print('--- FAILURE! showing running config')
+    exit()
+check_hardening(running-config)
 
 session.sendline('configure terminal')
 result = session.expect([r'.\(config\)#', pexpect.TIMEOUT, pexpect.EOF])
